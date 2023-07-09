4cc621fbcdd8c92b3e536fc39d2c153eb8669aa6
Ercan KAYA <72192523+ercankya@users.noreply.github.com>
2023-07-09 15:01:26 +0300

    Create 4.ödevCevapları.sql

---
+++ 4.ödevCevapları.sql
@@ -0,0 +1,12 @@
+--select distinct replacement_cost from film
+
+--select count(distinct replacement_cost) from film
+
+--select count(*) from film
+--where title like 'T%' and rating ='G'
+
+--select count(country) from country
+--where 
+
+--select count(*) from city
+--where city like 'r%' or city like 'R%'
