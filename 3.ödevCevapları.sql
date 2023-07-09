86b79c14e4ff5e53e748212558127491cf23e6f6
Ercan KAYA <72192523+ercankya@users.noreply.github.com>
2023-07-09 14:33:54 +0300

    Create 3.ödevCevapları.sql

---
+++ 3.ödevCevapları.sql
@@ -0,0 +1,15 @@
+--select country from country
+--where country like 'A%a'
+
+
+--select country from country
+--where country like '_____n'
+
+
+--select title from film 
+--where title like '%t%'
+
+
+select title from film 
+where title like 'C%' and rental_rate=2.99 and length>90
+
