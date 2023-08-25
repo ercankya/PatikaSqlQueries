5dfe2c3b2272956a3acf8a5223a74f94cd07818e
Ercan KAYA <72192523+ercankya@users.noreply.github.com>
2023-08-25 13:03:11 +0300

    Create 10.ÖdevCevapları.sql

---
+++ 10.ÖdevCevapları.sql
@@ -0,0 +1,19 @@
+
+city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country)
+isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+
+customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki
+first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+
+customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki
+first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+
+
+select city,country from city
+left join country on country.country_id = city.country_id;
+
+select payment_id, first_name, last_name from customer
+right join payment on payment.customer_id = customer.customer_id;
+
+select rental_id, first_name, last_name from customer
+full join rental on rental.customer_id = customer.customer_id;
